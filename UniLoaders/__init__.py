9f46e9013f6a8c966ba1a9e5ea82ba6fc88fee10
Enzo Bonelli <j4kr4bbit@gmail.com>
2023-10-25 02:05:55 -0500

    pushed 0.1.0

--- UniLoaders/__init__.py
+++ UniLoaders/__init__.py
@@ -1,6 +1,6 @@
 from langchain.document_loaders import Docx2txtLoader
 from langchain.document_loaders import UnstructuredPDFLoader
-
+from langchain.document_loaders import UnstructuredPowerPointLoader
 
 def all_loaders(path: str):
     if path.endswith("docx"):
@@ -8,5 +8,9 @@ def all_loaders(path: str):
         return data.load()
     
     if path.endswith("pdf"):
-        data=UnstructuredPDFLoader(path, mode='elements')
+        data=UnstructuredPDFLoader(path)
+        return data.load()
+    
+    if path.endswith("ppt"):
+        data=UnstructuredPowerPointLoader(path)
         return data.load()

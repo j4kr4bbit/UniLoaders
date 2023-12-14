319725a14b12448c70e4a681ecceef17fe04260d
Enzo Bonelli <j4kr4bbit@gmail.com>
2023-12-14 01:05:02 -0600

    pushed 0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='UniLoaders',
-    version='0.1.7',
+    version='0.1.8',
     description='Loading everything from URLs to powerpoints',
     author='Enzo Bonelli', 
     long_description=long_description,
